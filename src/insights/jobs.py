fa6c14d3d19e138f2fabd76ced45fa8aa4c95bb0
ioott <vioott@gmail.com>
2023-01-24 10:43:35 -0300

    feat: adiciona a função get_unique_job_types em jobs.py esta função é responsável por gerar uma lista com os tipos de trabalho existentes

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,30 +5,22 @@ import csv
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    result = []
+    jobs = []
     with open(path, "r") as file:
         DictReader_obj = csv.DictReader(file)
         for item in DictReader_obj:
-            result.append(dict(item))
-        return result
+            jobs.append(dict(item))
+        return jobs
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    jobs = []
+    with open(path, "r") as file:
+        DictReader_obj = csv.DictReader(file)
+        for item in DictReader_obj:
+            if item["job_type"] not in jobs:
+                jobs.append(dict(item)["job_type"])
+        return jobs
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
