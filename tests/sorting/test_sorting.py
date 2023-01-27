52bf58809999ec876ad1b611f8f2cc7961be76af
ioott <vioott@gmail.com>
2023-01-26 21:12:33 -0300

    test: adiciona o teste da função sort_by em sorting.py
    O teste visa garantir que a pessoa usuária possa escolher um critério de ordenação para exibir os empregos.

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,26 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+import pytest
 
 
+@pytest.fixture
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"min_salary": 1300, "max_salary": 2300, "date_posted": "2023-01-02"},
+        {"min_salary": 1500, "max_salary": 2500, "date_posted": "2022-10-18"},
+        {"min_salary": 1800, "max_salary": 2800, "date_posted": "2023-01-01"},
+    ]
+
+    expect = [
+        [jobs[0], jobs[1], jobs[2]],
+        [jobs[2], jobs[1], jobs[0]],
+        [jobs[0], jobs[2], jobs[1]],
+    ]
+
+    sort_by(jobs, "min_salary")
+    assert jobs == expect[0]
+
+    sort_by(jobs, "max_salary")
+    assert jobs == expect[1]
+
+    sort_by(jobs, "date_posted")
+    assert jobs == expect[2]
