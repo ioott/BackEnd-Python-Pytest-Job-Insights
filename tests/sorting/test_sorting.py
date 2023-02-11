57ea65530c6e3284daf77fedb9734f78834c236d
ioott <vioott@gmail.com>
2023-02-11 20:20:23 -0300

    test: adiciona o teste da função sor_by_criteria, que gera o relatório de acordo com os critérios definidos pelo usuário

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,8 +1,6 @@
 from src.pre_built.sorting import sort_by
-import pytest
 
 
-@pytest.fixture
 def test_sort_by_criteria():
     jobs = [
         {"min_salary": 1300, "max_salary": 2300, "date_posted": "2023-01-02"},
@@ -10,17 +8,17 @@ def test_sort_by_criteria():
         {"min_salary": 1800, "max_salary": 2800, "date_posted": "2023-01-01"},
     ]
 
-    expect = [
+    result = [
         [jobs[0], jobs[1], jobs[2]],
         [jobs[2], jobs[1], jobs[0]],
         [jobs[0], jobs[2], jobs[1]],
     ]
 
     sort_by(jobs, "min_salary")
-    assert jobs == expect[0]
+    assert jobs == result[0]
 
     sort_by(jobs, "max_salary")
-    assert jobs == expect[1]
+    assert jobs == result[1]
 
     sort_by(jobs, "date_posted")
-    assert jobs == expect[2]
+    assert jobs == result[2]
